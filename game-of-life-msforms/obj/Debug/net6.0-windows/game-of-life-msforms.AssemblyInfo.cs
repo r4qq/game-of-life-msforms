d8bfbc27dd11d4433abbda7ba0f054ddfa2f92f3
R4qq <r4qq@protonmail.com>
2025-05-13 19:57:42 +0200

    for readme

--- game-of-life-msforms/obj/Debug/net6.0-windows/game-of-life-msforms.AssemblyInfo.cs
+++ game-of-life-msforms/obj/Debug/net6.0-windows/game-of-life-msforms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("game-of-life-msforms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09283fba06fb0a5eee459a89effbbf74533f6178")]
 [assembly: System.Reflection.AssemblyProductAttribute("game-of-life-msforms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("game-of-life-msforms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
